77f2cb564ef3d6a63f66030f2d43ab358c4f285d
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-06-04 12:18:30 +0530

    improve input field

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button") 
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
     push(shoppingListInDB, inputValue)
+
+    inputFieldEl.value = ""
+
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 
     console.log(inputValue)
 })
