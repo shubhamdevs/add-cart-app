bbd8fd441ae9530bfa00a239c00c7bee28a942ba
SHUBHAM BISWAS <suvm.dev@gmail.com>
2023-06-04 12:09:16 +0530

    add functions to button

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"  
+
+const appSettings = {
+    databaseURL : "https://realtime-database-83e88-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button") 
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+
+    push(shoppingListInDB, inputValue)
+
     console.log(inputValue)
 })
